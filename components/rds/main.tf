8fb8cac57ffcc85816114cee49cc00a06b84cd23
jbruce-nex <james.bruce@ba.com>
2024-04-03 16:21:36 +0100

    update

--- components/rds/main.tf
+++
@@ -1,4 +0,0 @@
-resource "aws_instance" "web" {
-  ami           = "ami-087c3138d6ccce963"
-  instance_type = "t2.micro"
-}
---
+++ components/rds/variables.tf
@@ -0,0 +1,55 @@
+##
+# Basic Required Variables for tfscaffold Components
+##
+
+variable "project" {
+  type        = string
+  description = "The tfscaffold project"
+}
+
+variable "aws_account_id" {
+  type        = string
+  description = "The AWS Account ID (numeric)"
+}
+
+variable "region" {
+  type        = string
+  description = "The AWS Region"
+}
+
+variable "group" {
+  type        = string
+  description = "The group variables are being inherited from (often synonymous with account short-name)"
+  default     = ""
+}
+
+variable "environment" {
+  type        = string
+  description = "The environment variables are being inherited from"
+}
+
+##
+# tfscaffold variables specific to this component
+##
+
+# This is the only primary variable to have its value defined as
+# a default within its declaration in this file, because the variables
+# purpose is as an identifier unique to this component, rather
+# then to the environment from where all other variables come.
+variable "component" {
+  type        = string
+  description = "The variable encapsulating the name of this component"
+  default     = "core"
+}
+
+variable "default_tags" {
+  type        = map(string)
+  description = "A map of default tags to apply to all taggable resources within the component"
+  default     = {}
+}
+
+variable "deployment_execution_role_name" {
+  type        = string
+  description = "The role name for terraform to assume to perform all deployment actions in a given account. This is determined by the Halo Landing Zone and should not normally be changed."
+  default     = "DeploymentExecution"
+}
