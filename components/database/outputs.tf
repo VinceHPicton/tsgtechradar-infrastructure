36912d1f14f71b08df560a7f73afad983d2d78a7
jbruce-nex <james.bruce@ba.com>
2024-04-08 11:40:38 +0100

    update

--- components/database/outputs.tf
+++ components/database/outputs.tf
@@ -3,7 +3,7 @@ output "tags" {
   value       = local.default_tags
 }
 
-output "test_local" {
-  description = "View output of local private_subnet_cidrs"
-  value       = local.private_subnet_cidrs
+output "database_hostname" {
+  description = "Endpoint of the DB"
+  value       = module.rds_db.db_instance_endpoint
 }
