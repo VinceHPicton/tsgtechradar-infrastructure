9a955a94055c252cca8f24ff1e6aad09eae132bc
jbruce-nex <james.bruce@ba.com>
2024-04-09 15:23:25 +0100

    update webport

--- components/host/variables.tf
+++ components/host/variables.tf
@@ -64,7 +64,7 @@ variable "vpc_id" {
 variable "web_port" {
   type        = string
   description = "Unprivileged port for the web app"
-  default     = "8080"
+  default     = "3000"
 }
 
 variable "jira_ssm_parameter_name" {
