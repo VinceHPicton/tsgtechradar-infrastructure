1479b72dd02634f15b6d983bb43b077af53a3f2c
Jack Wilson <jack.wilson@bjss.com>
2024-06-17 13:05:14 +0100

    add healthy host alarm

---
+++ components/host/alarms.tf
@@ -0,0 +1,18 @@
+resource "aws_cloudwatch_metric_alarm" "healthyhosts" {
+  alarm_name          = "Unhealthy Hosts"
+  comparison_operator = "LessThanThreshold"
+  evaluation_periods  = 1
+  metric_name         = "HealthyHostCount"
+  namespace           = "AWS/NetworkELB"
+  period              = 120
+  statistic           = "Average"
+  threshold           = 1
+  alarm_description   = "Number of healthy nodes in Target Group"
+  actions_enabled     = "true"
+  alarm_actions       = [aws_sns_topic.healthyhosts.arn]
+  ok_actions          = [aws_sns_topic.healthyhosts.arn]
+  dimensions = {
+    TargetGroup  = module.alb.target_groups[arn]
+    LoadBalancer = module.alb.arn_suffix
+  }
+}
---
+++ components/host/sns.tf
@@ -0,0 +1,49 @@
+resource "aws_sns_topic" "healthyhosts" {
+  name = "healthy_hosts_below_threshold"
+}
+
+resource "aws_sns_topic_policy" "healthyhosts" {
+  arn = aws_sns_topic.healthyhosts.arn
+
+  policy = data.aws_iam_policy_document.sns_topic_policy.json
+}
+
+data "aws_iam_policy_document" "healthyhosts" {
+  policy_id = "__default_policy_ID"
+
+  statement {
+    actions = [
+      "SNS:Subscribe",
+      "SNS:SetTopicAttributes",
+      "SNS:RemovePermission",
+      "SNS:Receive",
+      "SNS:Publish",
+      "SNS:ListSubscriptionsByTopic",
+      "SNS:GetTopicAttributes",
+      "SNS:DeleteTopic",
+      "SNS:AddPermission",
+    ]
+
+    condition {
+      test     = "StringEquals"
+      variable = "AWS:SourceOwner"
+
+      values = [
+        var.aws_account_id,
+      ]
+    }
+
+    effect = "Allow"
+
+    principals {
+      type        = "AWS"
+      identifiers = ["*"]
+    }
+
+    resources = [
+      aws_sns_topic.healthyhosts.arn,
+    ]
+
+    sid = "__default_statement_ID"
+  }
+}
